df26edabfccd0e6b5271f24c2ea86b3d1ed07a57
MLA98 <yuldong@ucdavis.edu>
2019-05-28 15:16:48 -0700

    Home page is implemented

---
+++ 189GAME/Assets/Resources/Scripts/WelcomeButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WelcomeButtons : MonoBehaviour
+{
+    // Start the game
+    public void SwtichToGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // Quit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
