e0b13dc570a6ddad3f95e90f707df8af8118e168
Ahmed Baki <baki95@outlook.com>
2019-05-28 14:24:18 -0700

    Add command pattern with player controller and commands

---
+++ 189GAME/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Player.Command;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float PlayerSpeed;
+        private IPlayerCommand Clockwise;
+        private IPlayerCommand CounterClockwise;
+
+        void Start()
+        {
+            this.Clockwise = ScriptableObject.CreateInstance<MovePlayerClockwise>();
+            this.CounterClockwise = ScriptableObject.CreateInstance<MovePlayerCounterClockwise>();
+        }
+
+        void FixedUpdate()
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0)
+            {
+                this.Clockwise.Execute(this.gameObject);
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                this.CounterClockwise.Execute(this.gameObject);
+            }
+        }
+    }
+}
