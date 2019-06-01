6488ae4f72a5a414924ee065e5295f92c9052eb0
elf33032 <49417714+elf33032@users.noreply.github.com>
2019-06-01 15:38:18 -0700

    Added Player Jump, but it still has some bugs

--- 189GAME/Assets/Resources/Scripts/PlayerController.cs
+++ 189GAME/Assets/Resources/Scripts/PlayerController.cs
@@ -8,14 +8,22 @@ namespace Player
         private IPlayerCommand Clockwise;
         private IPlayerCommand CounterClockwise;
         private IPlayerCommand Shoot;
+        private IPlayerCommand Jump;
         [SerializeField]
         private AudioSource ShootSound;
+        private float Timer;
+        private enum JumpState { CD, Ready};
+        private JumpState curr;
+        private float JumpTime = 0.2f;
+        private float JumpCD = 0.5f;
 
         void Start()
         {
             this.Clockwise = ScriptableObject.CreateInstance<MovePlayerClockwise>();
             this.CounterClockwise = ScriptableObject.CreateInstance<MovePlayerCounterClockwise>();
             this.Shoot = ScriptableObject.CreateInstance<PlayerShoot>();
+            this.Jump = ScriptableObject.CreateInstance<PlayerJump>();
+            Timer = 0.0f;
         }
 
         void FixedUpdate()
@@ -37,6 +45,25 @@ namespace Player
                 {
                     this.Shoot.Execute(this.gameObject);
                     ShootSound.Play();
+                }
+                if (Input.GetButton("Fire1") && curr == JumpState.Ready)
+                {   
+                    this.Jump.Execute(this.gameObject);
+                    Timer += Time.deltaTime;
+                    if(Timer >= JumpTime)
+                    {
+                        curr = JumpState.CD;
+                        Timer = 0;
+                    }
+                }
+                else if(curr == JumpState.CD)
+                {
+                    Timer += Time.deltaTime;
+                    if(Timer >= JumpCD)
+                    {
+                        curr = JumpState.Ready;
+                        Timer = 0;
+                    }
                 }
             }
         }
---
+++ 189GAME/Assets/Resources/Scripts/PlayerJump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using Player.Command;
+
+namespace Player.Command
+{
+    public class PlayerJump : ScriptableObject, IPlayerCommand
+    {
+        private float JumpSpeed = 50f;
+
+
+        public void Execute(GameObject gameObject)
+        {
+            var Mars = GameObject.Find("Mars");
+            var body = gameObject.transform;
+            Vector3 gravityUp = (body.position - Mars.transform.position).normalized;
+            Vector3 bodyUp = body.up;
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            if(rigidBody != null)
+            {
+                rigidBody.AddForce(rigidBody.transform.up * JumpSpeed);
+                Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+                body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 100 * Time.deltaTime);
+            }
+        }
+    }
+}
