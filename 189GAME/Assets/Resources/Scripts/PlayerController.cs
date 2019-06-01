b3476f84027d133c386e090340bab4828fe88053
elf33032 <49417714+elf33032@users.noreply.github.com>
2019-06-01 16:04:31 -0700

    Fixed PlayerJump Controll

--- 189GAME/Assets/Resources/Scripts/PlayerController.cs
+++ 189GAME/Assets/Resources/Scripts/PlayerController.cs
@@ -11,11 +11,6 @@ namespace Player
         private IPlayerCommand Jump;
         [SerializeField]
         private AudioSource ShootSound;
-        private float Timer;
-        private enum JumpState { CD, Ready};
-        private JumpState curr;
-        private float JumpTime = 0.2f;
-        private float JumpCD = 0.5f;
 
         void Start()
         {
@@ -46,25 +41,14 @@ namespace Player
                     this.Shoot.Execute(this.gameObject);
                     ShootSound.Play();
                 }
-                if (Input.GetButton("Fire1") && curr == JumpState.Ready)
-                {   
-                    this.Jump.Execute(this.gameObject);
-                    Timer += Time.deltaTime;
-                    if(Timer >= JumpTime)
-                    {
-                        curr = JumpState.CD;
-                        Timer = 0;
-                    }
-                }
-                else if(curr == JumpState.CD)
-                {
-                    Timer += Time.deltaTime;
-                    if(Timer >= JumpCD)
-                    {
-                        curr = JumpState.Ready;
-                        Timer = 0;
-                    }
-                }
+            }
+        }
+        private void OnCollisionStay(Collision other)
+        {
+            if (other.gameObject.tag == "Planet" && Input.GetButton("Fire1"))
+            {
+                Debug.Log("Called");
+                this.Jump.Execute(this.gameObject);
             }
         }
     }
