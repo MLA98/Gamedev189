f0a4beefd45eaa74fefec1b457e38c92d17c4e6a
elf33032 <49417714+elf33032@users.noreply.github.com>
2019-06-02 17:42:13 -0700

    Add files via upload

---
+++ 189GAME/Assets/Resources/Scripts/SShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SShipController : MonoBehaviour
+{
+    private Vector3 Target;
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private float Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = new Vector3(Random.Range(-5, 4), 0, Random.Range(-6, 5));
+        Debug.Log(Target);
+        this.transform.LookAt(Target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.currState == GameManager.gameState.playing)
+        {
+            this.transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+
+    }
+}
---
+++ 189GAME/Assets/Resources/Scripts/SShipSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SShipSpawner : MonoBehaviour
+{
+    public GameObject SShipPrefab;
+
+    private float spawnRate;
+    private float timer;
+    private float SShipCounter;
+    private float waveLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveLimit = GameManager.instance.Wave * 2;
+        spawnRate = 10 + 0.1f * (GameManager.instance.Wave - 1);
+        timer += Time.deltaTime;
+        if (timer >= spawnRate && GameManager.instance.currState == GameManager.gameState.playing && SShipCounter <= waveLimit)
+        {
+            Vector3 offset;
+            offset.x = Random.value > 0.5f ?
+                Random.Range(-8f, -10f) :
+                Random.Range(8f, 10f);
+            offset.y = 0f;
+            offset.z = Random.value > 0.5f ?
+                Random.Range(-10f, -12f) :
+                Random.Range(10f, 12f);
+            var SShip = (GameObject)Instantiate(SShipPrefab, offset, Quaternion.identity);
+            timer = 0f;
+            SShipCounter += 1;
+        }
+        if (SShipCounter > waveLimit && GameObject.FindGameObjectsWithTag("SShip").Length == 0)
+        {
+            GameManager.instance.currState = GameManager.gameState.waveCompleted;
+            SShipCounter = 0;
+        }
+    }
+}
