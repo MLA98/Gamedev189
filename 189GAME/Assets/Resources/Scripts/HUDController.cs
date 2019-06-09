574b7593232978299fe7b9405954d6063373359e
Ahmed Baki <baki95@outlook.com>
2019-06-09 11:37:55 -0700

    Add Mobile HUD dependencies

---
+++ 189GAME/Assets/Resources/Scripts/HUDController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject HUDGameObject;
+    GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = GameManager.Instance;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instance.currState == GameManager.gameState.playing)
+        {
+            HUDGameObject.SetActive(true);
+        } else
+        {
+            HUDGameObject.SetActive(false);
+        }
+    }
+}
